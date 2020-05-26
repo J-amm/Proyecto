304ed89c412e72d39f0aaa38aa74aaed23b93e48
J-amm <65968574+J-amm@users.noreply.github.com>
2020-05-26 11:17:37 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+function mostrar(){
+	document.getElementById('siguiente').style.display="block";
+}
+function ocultar(){
+	document.getElementById('siguiente').style.display="none";
+}
---
+++ java2.js
@@ -0,0 +1,51 @@
+function mostrar_regreso(){
+document.getElementById('regresar').style.display="block";
+}
+function ocultar_regreso(){
+document.getElementById('regresar').style.display="none";
+}
+
+function mostrar(){
+document.getElementById('menu').style.display="block";
+}
+function ocultar(){
+document.getElementById('menu').style.display="none";
+}
+function mostrar1(){
+document.getElementById('Primer texto').style.display="block";
+}
+function ocultar1(){
+document.getElementById('Primer texto').style.display="none";
+}
+
+function mostrar2(){
+document.getElementById('Segundo texto').style.display="block";
+}
+function ocultar2(){
+document.getElementById('Segundo texto').style.display="none";
+}
+function mostrar3(){
+document.getElementById('Tercer texto').style.display="block";
+}
+function ocultar3(){
+document.getElementById('Tercer texto').style.display="none";
+}
+function mostrar4(){
+document.getElementById('Cuarto texto').style.display="block";
+}
+function ocultar4(){
+document.getElementById('Cuarto texto').style.display="none";
+}
+function mos_img1(){
+document.getElementById('img1').style.display="block";
+}
+function oc_img1(){
+document.getElementById('img1').style.display="none";
+}
+
+function mos_img2(){
+document.getElementById('img2').style.display="block";
+}
+function oc_img2(){
+document.getElementById('img2').style.display="none";
+}
